08f12b72c25e6734200445ab344de2681f457cc3
Jay Park <jhpark.main@gmail.com>
2023-04-10 00:48:42 +0900

    (feat) 전역상태 관리 및 문서 저장 기능 추가
    - 전역상태관리를 위해 Jotai를 설치함
    - 페이지 실행 시 최초 1회 로컬스토리지에 사전 정의된 문서 저장

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Header } from './components/header';
 import { LeftSideBar } from './components/left-sidebar';
 import { Feed } from './components/feed';
 import { RightSidebar } from './components/right-sidebar';
+import { DOCS } from './db/docs';
+import { useSetAtom } from 'jotai';
+import { DocsAtom } from './contexts/docsAtom';
 
 export const App = (): React.ReactElement => {
+  const setDocsAtom = useSetAtom(DocsAtom);
+
+  useEffect(() => {
+    //최초 1회 사전 정의된 문서를 가지고 와 localstorage에 저장합니다.
+    if (!localStorage.getItem('2pmdocs')) {
+      localStorage.setItem('2pmdocs', JSON.stringify(DOCS));
+    }
+
+    const currentDocs = localStorage.getItem('2pmdocs');
+    //로컬스토리지로부터 문서 데이터를 받아와 전역 상태로 지정합니다.
+    setDocsAtom(currentDocs ? JSON.parse(currentDocs) : []);
+  }, []);
+
   return (
     <div className="flex min-h-screen w-full justify-center bg-fbbg">
       <Header />
